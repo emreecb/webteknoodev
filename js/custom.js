9ef82fc7f2ffa67bf7fbf3b19203fed98329277d
emreecb <33958482+emreecb@users.noreply.github.com>
2018-03-02 19:30:42 +0300

    Başlangıç yüklemesi.

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+﻿
+$(document).ready(function () {
+    $("#BitisTarih").datepicker({ dateFormat: 'DD.MM.YYYY' });
+    $("#BitisTarih").datepicker('setDate', new Date());
+    $("#BaslangicTarih").datepicker({ dateFormat: 'DD.MM.YYYY' });
+    $("#BaslangicTarih").datepicker('setDate', new Date());
+});
