dd87139f717936ba566a8951f4e41cd4bc20c579
emreecb <33958482+emreecb@users.noreply.github.com>
2018-08-17 17:31:13 +0300

    next commit

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,1 @@
 ﻿
-$(document).ready(function () {
-    $("#BitisTarih").datepicker({ dateFormat: 'DD.MM.YYYY' });
-    $("#BitisTarih").datepicker('setDate', new Date());
-    $("#BaslangicTarih").datepicker({ dateFormat: 'DD.MM.YYYY' });
-    $("#BaslangicTarih").datepicker('setDate', new Date());
-});
